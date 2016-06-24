0b2573d6d4a894f6fc9f46330ca1a00533ec6cc7
RubyBe <sidbenavente@hotmail.com>
2016-06-24 09:43:47 -0700

    add ability to prioritize replacements

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,14 @@
 // Business logic
 
-// It can count up to the provided number and return the resulting string of numbers
-var countUp = function(number) {
-  for (var i = 0; i < number; i++) {
-    console.log(i + 1);
-  };
-};
+// // It can count up to the provided number and return the resulting string of numbers
+// var countUp = function(number) {
+//   for (var i = 0; i < number; i++) {
+//     console.log(i + 1);
+//   };
+// };
 
-// It can identify multiples of 3 within a given string of numbers and replace those values with "ping"
- // It can identify multiples of 5 within a given string of numbers and replace those values with "pong"
+// // It can identify multiples of 3 within a given string of numbers and replace those values with "ping"
+// // It can identify multiples of 5 within a given string of numbers and replace those values with "pong"
 // var replaceThrees = function(number) {
 //   myNumber = [];
 //   for (var i = 0; i < number; i++) {
@@ -24,26 +24,37 @@ var countUp = function(number) {
 //   };
 // };
 
+// // It can identify multiples of 15 within a given string of numbers and replace those values with "ping-pong"
+// var replaceThrees = function(number) {
+//   myNumber = [];
+//   for (var i = 0; i < number; i++) {
+//     if ((i + 1) % 15 === 0) {
+//       myNumber.push("ping-pong");
+//       } else {
+//       myNumber.push(i + 1);
+//     }
+//     console.log(myNumber);
+//   };
+// };
 
-// It can identify multiples of 15 within a given string of numbers and replace those values with "ping-pong"
-var replaceThrees = function(number) {
+// It can prioritize conflicts by understanding that multiples of 5 trump multiples of 3, and multiples of 15 trump multiples of 5, and so when given a string of numbers it can appropriately replace multiples of 3, 5, and 15 with "ping", "pong", and "ping-pong", respectively
+var replaceAll = function(number) {
   myNumber = [];
   for (var i = 0; i < number; i++) {
-    if ((i + 1) % 15 === 0) {
-      myNumber.push("ping-pong");
-      } else {
-      myNumber.push(i + 1);
+    if (((i + 1) % 3 === 0) && ((i + 1) % 5 !== 0) && ((i + 1) % 15 !== 0)) {
+      myNumber.push("ping");
+    } else if
+      (((i + 1) % 5 === 0) && ((i + 1) % 15 !== 0)) {
+        myNumber.push("pong");
+      } else if
+        ((i + 1) % 15 === 0) {
+          myNumber.push("ping-pong");
+        } else {
+          myNumber.push(i + 1);
     }
-    console.log(myNumber);
   };
+  console.log(myNumber);
 };
-
-
-
-
-
-
-
 
 
 
@@ -57,8 +68,8 @@ $(document).ready(function() {
   $("#input").submit(function(event) {
     event.preventDefault();
     var userInput = parseInt($("#input_text").val());
-    countUp(userInput);
-    replaceThrees(userInput);
+
+    replaceAll(userInput);
 
     // var output = rome(userInput);
     // $(".calculated_output").text(output);
